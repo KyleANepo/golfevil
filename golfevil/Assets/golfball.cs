32343ad714a76bcca4a55f89b622697d5b72de3d
KyleANepo <kyle.nepo@wsu.edu>
2024-08-14 02:29:29 -0700

    added golf systme
    added golf systme

--- golfevil/Assets/golfball.cs
+++ golfevil/Assets/golfball.cs
@@ -15,12 +15,37 @@ public class golfball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Launch(100f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(0.0f, 0.0f, 5.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(0.0f, 0.0f, -5.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
+    public void Reset()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+
+    public void RotateBall(float angle)
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
     }
 
     // launch ball in direction based on angle and power
-    public void Launch()
+    public void Launch(float force)
     {
-
+        body.AddForce(transform.up * force);
     }
 }
